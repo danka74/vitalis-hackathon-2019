79ce8d3d07c59500ef724a251f8ccab60d9c3094
danka74 <daniel.karlsson@socialstyrelsen.se>
2018-07-06 17:00:33 +0200

    added side nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,30 @@ import { HttpClientModule } from '@angular/common/http';
 import { SnomedService } from './snomed.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule, MatFormFieldModule, MatAutocompleteModule, MatOptionModule, MatCheckboxModule } from '@angular/material';
+import { MatInputModule, MatFormFieldModule, MatAutocompleteModule, MatOptionModule, MatCheckboxModule,
+  MatToolbarModule, MatButtonModule, MatSidenavModule, MatListModule } from '@angular/material';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { SnomedAutocompleteComponent } from './snomed-autocomplete/snomed-autocomplete.component';
+import { MyNavComponent } from './my-nav/my-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SnomedAutocompleteComponent
+    SnomedAutocompleteComponent,
+    MyNavComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule, ReactiveFormsModule,
     BrowserAnimationsModule,
-    MatInputModule, MatFormFieldModule, MatAutocompleteModule, MatOptionModule, MatChipsModule, MatIconModule, MatCheckboxModule
+    MatInputModule, MatFormFieldModule, MatAutocompleteModule, MatOptionModule, MatChipsModule, MatIconModule,
+    MatCheckboxModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatListModule
   ],
   providers: [
     SnomedService
