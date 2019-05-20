a8944b70f04c7c75ff50fd9aa7bfd90c98cb35ea
Daniel Karlsson <daniel.karlsson@socialstyrelsen.se>
2019-05-20 09:15:44 +0200

    Update snomed.service.ts

--- src/app/snomed.service.ts
+++ src/app/snomed.service.ts
@@ -1,32 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { of } from 'rxjs';
 
 
 @Injectable()
 export class SnomedService {
 
-  readonly baseUrl: string = "http://localhost:4200/fhir/ValueSet/$expand?url=http://snomed.info/sct/45991000052106?fhir_vs=ecl/";
-
   constructor(private http: HttpClient) { }
 
   search(term: string, ecl: string): any {
 
-    console.log(ecl);
-
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type':  'application/json',
-      })
-    };
-    if (term && term.length > 0) {
-      return this.http.get(this.baseUrl +
-         ((ecl != null && ecl !== '') ? encodeURIComponent(ecl) : '<<404684003') + // default to Clinical finding
-         '&displayLanguage=sv&count=15' +
-         '&filter=' +
-         encodeURIComponent(term));
-    } else {
-        return [];
-    }
+    return of('248281009 |oklart sjukdomstillstånd|');
   }
 
 }
